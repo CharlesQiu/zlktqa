d22f5ce1d444cb3ade32b2871a9ffb818a456ef6
Charles Qiu <qhs@outlook.com>
2018-07-23 18:00:56 +0800

    项目结构搭建“

--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-#enconding: utf-8
+# enconding: utf-8
 
 from flask_script import Manager
 from flask_migrate import MigrateCommand, Migrate
@@ -11,5 +11,5 @@ migrate = Migrate(app, db)
 
 migrate.add_command('db', MigrateCommand)
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     manager.run()
